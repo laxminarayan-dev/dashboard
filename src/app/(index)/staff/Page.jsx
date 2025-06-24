d5bddb709e134c67678b59d2aa2e2445b62bc3d1
Laxmi Narayan <jaiswallucky0112@gmail.com>
2025-06-24 19:09:55 +0530

    deployment tessting

--- src/app/(index)/staff/Page.jsx
+++ src/app/(index)/staff/Page.jsx
@@ -3,7 +3,8 @@ const StaffManagement = () => {
     <div>
       <h1>Staff Management</h1>
       <p>Manage your staff members here.</p>
-      {/* Additional components or functionality can be added here */}
+      {/* Additional components or functionality can be added here */}update
+      staff
     </div>
   );
 };
