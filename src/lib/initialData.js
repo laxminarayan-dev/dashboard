ae820ee672649655589a17fd5c192245cd2a3b4f
Laxmi Narayan <jaiswallucky0112@gmail.com>
2025-07-10 18:46:43 +0530

    lib created for global data

---
+++ src/lib/initialData.js
@@ -0,0 +1,23 @@
+export const ordersInitialData = {
+    amount: 0,
+    customerName: "",
+    deliveredDateTime: "",
+    orderDateTime: "",
+    orderId: "",
+    orderItem: "",
+    paymentMethod: "",
+    quantity: 0,
+    status: "",
+    deliveredAddress: "",
+};
+
+export const transactionsInitialData = {
+    amount: 0,
+    transactionId: "",
+    status: "",
+    transactionDateTime: "",
+    method: "",
+    reference: "",
+    type: "",
+    userName: "",
+};
